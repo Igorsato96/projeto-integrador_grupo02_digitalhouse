60d8e94962f5ed152ceb70947fde0f840d9e3dee
Igorsato96 <62575213+Igorsato96@users.noreply.github.com>
2020-04-30 17:49:23 -0300

    projeto
    front-end

---
+++ Projeto/js/main.js
@@ -0,0 +1,16 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptativeHeigth: true,
+    autoplay: true,
+    autoplayspeed: 10
+});
+
+$('.slider-secundario').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+
+});
